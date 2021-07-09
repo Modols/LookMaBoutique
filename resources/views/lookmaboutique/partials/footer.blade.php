850c9ba02fe24d877f519deed514d4a8fc459c43
Modols <thomas.modolo@ynov.com>
2021-07-09 16:03:05 +0200

    adding footer to accueil Page

--- resources/views/lookmaboutique/partials/footer.blade.php
+++ resources/views/lookmaboutique/partials/footer.blade.php
@@ -1,5 +1,14 @@
-<footer class="footer bg-primary">
-    <div class="container">
-        <span class="">Place sticky footer content here.</span>
+<div class="accueilFooter">
+    <div class="container-fluid">
+        {{-- <div class="accueilFooterlookmaboutique"> --}}
+            <div class="row">
+                <div class="col-8"><span style="color: rebeccapurple">Look</span>maboutique</div>
+                <div class="col-4 accueilFooterSocialMedia">
+                    <a href="https://fr-fr.facebook.com/" target="_blank"><img src="{{url('/images/logo_facebook.svg')}}" alt=""></a>
+                    <a href="https://www.instagram.com/?hl=fr"></a>
+                    <a href="https://www.linkedin.com/home/?originalSubdomain=fr"></a>
+                </div>
+            </div>
+        {{-- </div> --}}
     </div>
-</footer>
+</div>
