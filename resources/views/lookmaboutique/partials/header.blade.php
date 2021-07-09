5997468d7b10ba351417f5bc6e88d8c2c94f96e5
ZiKo <lucas.matas@ynov.com>
2021-07-09 15:27:23 +0200

    Mise a jour de bootstrap 5

--- resources/views/lookmaboutique/partials/header.blade.php
+++ resources/views/lookmaboutique/partials/header.blade.php
@@ -1,13 +1,13 @@
     <nav class="navbar accueilNavbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#"><span class="text-primary">Look</span>maboutique</a>
+        <a class="navbar-brand fs-2" href="#"><span class="text-primary fs-2">Look</span>maboutique</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+                    <a class="nav-link  fs-1" aria-current="page" href="#">Accueil</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Créations</a>
