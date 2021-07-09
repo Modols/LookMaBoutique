b3ec81f2bfbe6ce369bef5985dfd1e338b3ad176
Modols <thomas.modolo@ynov.com>
2021-07-09 13:54:46 +0200

    clearing some codes on Accueil Page

--- resources/views/lookmaboutique/accueil.blade.php
+++ resources/views/lookmaboutique/accueil.blade.php
@@ -4,46 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <script src="{{ asset('js/app.js') }}" defer></script>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <style>
-        
-    </style>
     <title>Lookmaboutique</title>
 </head>
 <body>
-
-    <div class="myHeader">
-        <nav class="navbar  navbar-expand-lg navbar-light bg-primary " style="font-size: 30px; z-index: 1">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Lookmaboutique</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Accueil</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Créations</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">A propos</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </div>
     
-    {{-- <div id="headerAccueil" >
+    <div class="accueilHeader">
         @include('lookmaboutique.partials.header')  
-        
     </div>
-
-    @include('lookmaboutique.partials.footer') --}}
     
+    @include('lookmaboutique.partials.footer')
 </body>
 </html>
--- resources/views/lookmaboutique/partials/header.blade.php
+++ resources/views/lookmaboutique/partials/header.blade.php
@@ -1,4 +1,4 @@
-    <nav class="navbar  navbar-expand-lg navbar-light bg-primary " style="font-size: 30px; z-index: 1">
+    <nav class="navbar accueilNavbar navbar-expand-lg navbar-light bg-primary">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">Lookmaboutique</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
