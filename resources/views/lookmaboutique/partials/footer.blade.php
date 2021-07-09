eac7de0b69060eff421c3c62d5c0b0fad2c67b83
Modols <thomas.modolo@ynov.com>
2021-07-09 17:55:16 +0200

    adding footer

--- resources/views/lookmaboutique/partials/footer.blade.php
+++ resources/views/lookmaboutique/partials/footer.blade.php
@@ -1,14 +1,46 @@
 <div class="accueilFooter">
     <div class="container-fluid">
-        {{-- <div class="accueilFooterlookmaboutique"> --}}
-            <div class="row">
-                <div class="col-8"><span style="color: rebeccapurple">Look</span>maboutique</div>
-                <div class="col-4 accueilFooterSocialMedia">
-                    <a href="https://fr-fr.facebook.com/" target="_blank"><img src="{{url('/images/logo_facebook.svg')}}" alt=""></a>
-                    <a href="https://www.instagram.com/?hl=fr"></a>
-                    <a href="https://www.linkedin.com/home/?originalSubdomain=fr"></a>
-                </div>
+        <div class="row" style="font-size: 30px;">
+            <div class="col-8 accueilFooterLook"><span style="color: rebeccapurple">Look</span>maboutique</div>
+            <div class="col-4 accueilFooterSocialMedia">
+                <a class="accueilFooterA" href="https://fr-fr.facebook.com/" target="_blank"><img src="{{url('/images/logo_facebook.svg')}}" alt="Facebook logo" width="35" height="35"></a>
+                <a class="accueilFooterA" href="https://www.instagram.com/?hl=fr"><img src="{{url('/images/logo_facebook.svg')}}" alt="Instagram logo" width="35" height="35"></a>
+                <a class="accueilFooterA" href="https://www.linkedin.com/home/?originalSubdomain=fr"><img src="{{url('/images/logo_facebook.svg')}}" alt="Linkedin logo" width="35" height="35"></a>
             </div>
-        {{-- </div> --}}
+        </div>
+        
+    </div>
+    <div class="accueilFooterHR"></div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-3 accueilFooterFlex">
+                <h1>Entreprise</h1>
+                <h4>Contactez-nous</h4>
+                <h4>A propos</h4>
+            </div>
+            <div class="col-2 accueilFooterFlex">
+                <h1>Entreprise</h1>
+                <h4>Creer mon site</h4>
+                <h4>Tester</h4>
+            </div>
+            <div class="col-3 accueilFooterFlex">
+                <h1>Compte</h1>
+                <h4>Mon profil</h4>
+                <h4>Mon site</h4>
+            </div>
+            <div class="col-1 accueilFooterFlex">
+                <span class="vertical-lineFooter"></span>
+            </div>
+            <div class="col-3 accueilFooterFlex">
+                <h1>Mention legales</h1>
+                <h1>Confidentialite</h1>
+            </div>
+        </div>
+    </div>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-8">Copyright C 2021 Lookmaboutique. All rights reserved</div>
+            <div class="col-4" style="text-align: right">Created by Matas / Modolo</div>
+        </div>
     </div>
 </div>
